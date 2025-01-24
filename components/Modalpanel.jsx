0da64e6ce515ef74e4cfe3e4c14773419c13b4dd
Agustin <latierramedia@gmail.com>
2025-01-24 15:37:18 +0100

    avances del curso 73

---
+++ components/Modalpanel.jsx
@@ -0,0 +1,21 @@
+export default function Modalpanel(){
+
+    return(
+        <>        
+            <div id="ModalPanel" className="modal-object">
+                <div className="modal-link-close" onclick="$('#ModalPanel').removeclassName('open');$('#ModalPanel-overlay').removeclassName('open');$('input#search').val('');$('ul#searchresults').html('');"></div>
+                <div id="ModalPanel-component" className="modal-content">
+                    <div className="search-results">
+                        <section>
+                            <h2 id="tituloresults" className="tituloresults">Resultados encontrados:</h2>
+                            <ul id="searchresults" className="searchresults">
+                            </ul>
+                        </section>
+                    </div>
+
+                </div>
+            </div>
+            <div id="ModalPanel-overlay" className="modal-overlay" onclick="$('#ModalPanel').removeclassName('open');$('#ModalPanel-overlay').removeclassName('open');$('input#search').val('');$('ul#searchresults').html('');"></div>
+        </>
+    )
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-
+import { useState, useEffect } from "react"
+import Modalpanel from "../components/ModalPanel"
 
 function App() {
 
 
   return (
     <>
-      
+      <Modalpanel/>
       
     </>
   )
